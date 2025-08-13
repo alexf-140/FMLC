1bf493cb919d1bce4b3ab361f03083434e47063c
alexf140 <alexf141214@gmail.com>
2025-08-13 17:19:32 +0300

    simple java adding two n# by user input

--- App.java
+++ App.java
@@ -1,22 +1,17 @@
 import java.util.*;
-import java.awt.*;
 
 public class App{
-        
-    public static int multiply(int x , int y){
-    int result = 0 ;
-    while(y != 0){
-        if((y & 1) != 0){
-            result += x;
-        }
-        x <<= 1;
-        y >>= 1;
-    } 
-    return result;       
-    }
-    public static void main(String[] args){
-       int result =  multiply(8,120);
-       System.out.println(result);
 
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two number");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = a + b;
+        System.out.println("Sum: "+ sum  );
     }
+
+
+
+
 }
