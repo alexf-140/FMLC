ef5977cdd4f36c5b790a90d7aaafd68ed0e3f1a5
alexf140 <alexf141214@gmail.com>
2025-08-14 09:48:53 +0300

    Making simple java adding 2 n# by user input with validation

--- App.java
+++ App.java
@@ -1,17 +1,39 @@
 import java.util.*;
 
 public class App{
-
+    
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter two number");
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int sum = a + b;
-        System.out.println("Sum: "+ sum  );
+        Scanner input = new Scanner(System.in);
+        int x =0 ,y = 0;
+        System.out.println("Enter the first number: ");
+        boolean validInput = false;
+        while(!validInput){
+        if(input.hasNextInt()){
+              x = input.nextInt();
+              validInput = true;
+        }
+        else{
+            System.err.println("Error, only integers are allowed !");
+            input.next();
+        }
     }
-
-
-
-
+        System.out.println("Enter the second number: ");
+        validInput = false;
+        while(!validInput){
+        if(input.hasNextInt()){
+              y = input.nextInt();
+              validInput = true;
+        }
+        else{
+            System.err.println("Error, only integers are allowed !");
+            input.next();
+        }
+    }
+        input.close();
+        System.out.println("The sum : " + add(x,y));
+        
+    }
+    public static int add(int x, int y){
+        return x + y;
+    }
 }
